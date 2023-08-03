adce8a2b31654dc8716fc6a353834587bfc2ac4d
genivalc <genivalcandeiadon.neto@gmail.com>
2023-08-03 11:51:55 -0300

    connect in DB

--- springboot2-essential/springboot2-essential/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2-essential/springboot2-essential/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -3,10 +3,20 @@
 package academy.devdojo.springboot2.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Data //Get e Set etc..
 @AllArgsConstructor //Cria constructor para todos
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private  String name;
 }
