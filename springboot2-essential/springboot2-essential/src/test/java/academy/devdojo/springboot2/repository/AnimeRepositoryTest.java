98b5baa67907b51ceb075d5c56be425d37932b1d
genivalc <genivalcandeiadon.neto@gmail.com>
2023-08-08 12:08:08 -0300

    Spring Anime Repository Test JPA Test

---
+++ springboot2-essential/springboot2-essential/src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,34 @@
+package academy.devdojo.springboot2.repository;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Test for Anime Repository")
+class AnimeRepositoryTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save creates anime when Sucessful")
+    void save_PersistAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());
+    }
+
+    private Anime createAnime(){
+        return Anime.builder()
+                .name("Teste")
+                .build();
+    }
+
+}
