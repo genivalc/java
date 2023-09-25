4f1e1550c38a78d7c2c0ae572f063bafb765ed1f
genivalc <genivalcandeiadon.neto@gmail.com>
2023-09-25 14:58:12 -0300

    created project email-service

---
+++ email-service/src/test/java/com/genival/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genival.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
