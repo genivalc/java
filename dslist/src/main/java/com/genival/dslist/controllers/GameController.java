3a571909ab3a4208db152eb6210abb442a231799
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-04 14:24:28 -0300

    get by id

--- dslist/src/main/java/com/genival/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/genival/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.genival.dslist.controllers;
 
+import com.genival.dslist.dto.GameDTO;
 import com.genival.dslist.dto.GameMinDTO;
 import com.genival.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
