f850ade8c6c6aa1847d8aa65202125581015875f
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-06 16:00:46 -0300

    Creat project Home-Brocker

---
+++ home.broker/src/test/java/com/genival/home/broker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genival.home.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
