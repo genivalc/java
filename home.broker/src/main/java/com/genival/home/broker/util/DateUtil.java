33bed20a08c825a1e2504dfa84a7291ca3e43ea9
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-09 16:03:44 -0300

    fix import @Component DateUtil

--- home.broker/src/main/java/com/genival/home/broker/util/DateUtil.java
+++ home.broker/src/main/java/com/genival/home/broker/util/DateUtil.java
@@ -1,8 +1,11 @@
 package com.genival.home.broker.util;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class DateUtil {
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
