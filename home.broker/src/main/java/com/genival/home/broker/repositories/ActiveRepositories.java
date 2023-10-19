7f71b25ca67c11f2e76f69f143b8d97bf08e937d
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-19 18:56:20 -0300

    feat ActiveRepositories

---
+++ home.broker/src/main/java/com/genival/home/broker/repositories/ActiveRepositories.java
@@ -0,0 +1,9 @@
+package com.genival.home.broker.repositories;
+
+import com.genival.home.broker.entities.Active;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActiveRepositories extends JpaRepository<Active, Long> {
+
+
+}
