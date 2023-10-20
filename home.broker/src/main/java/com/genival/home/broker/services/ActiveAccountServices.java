99221d5c32263ab2735fbd0e157959b915e8be81
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-20 17:12:32 -0300

    bug ActiveAccountServices

--- home.broker/src/main/java/com/genival/home/broker/services/ActiveAccountServices.java
+++ home.broker/src/main/java/com/genival/home/broker/services/ActiveAccountServices.java
@@ -4,7 +4,9 @@ import com.genival.home.broker.entities.Account;
 import com.genival.home.broker.entities.Active;
 import com.genival.home.broker.entities.ActiveAccount;
 import com.genival.home.broker.entities.Client;
+import com.genival.home.broker.repositories.AccountRepositories;
 import com.genival.home.broker.repositories.ActiveAccountRepositories;
+import com.genival.home.broker.repositories.ClientRepositories;
 import com.genival.home.broker.utils.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,6 +26,14 @@ import java.util.stream.Collectors;
 public class ActiveAccountServices {
     @Autowired
     private ActiveAccountRepositories activeAccountRepositories;
+
+    @Autowired
+    private AccountServices accountServices;
+
+
+    @Autowired
+    private ActiveServices activeServices;
+
 
     Util util = new Util();
 
@@ -46,19 +56,19 @@ public class ActiveAccountServices {
         return true;
     }
 
-//    @Transactional
-//    public void payDividends(String ticker, BigDecimal dividendPerShare) {
-//        List<AtivoConta> carteira = ativoContaRepository.findAll();
-//
-//        for (AtivoConta ativoConta : carteira) {
-//            if (!ativoConta.getConta().equals(contaRepository.findBolsaConta(contaRepository.findAll()))) {
-//                if (ativoConta.getAtivo().getTicker().equals(ticker)) {
-//                    BigDecimal totalDividends = dividendPerShare.multiply(new BigDecimal(ativoConta.getTotalAtivos()));
-//                    contaRepository.payDividends(contaRepository.findBolsaConta(contaRepository.findAll()), ativoConta.getConta(), totalDividends);
-//                }
-//            }
-//        }
-//    }
+    @Transactional
+    public void payDividends(String ticker, BigDecimal dividendPerShare, Client client) {
+        List<ActiveAccount> carteira = activeAccountRepositories.findAll();
+
+        for (ActiveAccount ativoConta : carteira) {
+            if (!ativoConta.getAccount().equals(accountServices.searchAccountStock(accountServices.findAll()))) {
+                if (ativoConta.getActive().getTicker().equals(ticker)) {
+                    BigDecimal totalDividends = dividendPerShare.multiply(new BigDecimal(ativoConta.getTotalActive()));
+                    accountServices.pagaDividendos(accountServices.searchAccountStock(accountServices.findAll()), ativoConta.getAccount(), totalDividends);
+                }
+            }
+        }
+    }
 
     public boolean isFull() {
         return activeAccountRepositories.count() == 0;
@@ -76,29 +86,21 @@ public class ActiveAccountServices {
         return activeAccountRepositories.findAllByContaAndAtivo(account, active);
     }
 
-    public List<ActiveAccount> getAllActiveAccount(ContaDAO contaDAO, AtivoDAO ativoDAO, ClienteDAO clienteDAO) {
-        List<ActiveAccount> alActiveAccount  =  searchAlActiveAccount();
+    public List<ActiveAccount> getAllActiveAccount(Client client) {
+        List<ActiveAccount> allActiveAccounts  =  searchAlActiveAccount();
 
-       return alActiveAccount.stream().map(rs -> {
-                   Long id = rs.getId();
-                   Long idAccount = rs.getAccount().getId();
-                   Long idActive = rs.getActive().getId();
-                   int totalActive = rs.getTotalActive();
-                   BigDecimal valorCompra = rs.getValorCompra();
-                   LocalDateTime dataCriacao = rs.getDateCreation();
-                   LocalDateTime dataModificacao = rs.getDateModification();
+        return allActiveAccounts.stream().map(activeAccount -> {
+            ActiveAccount ac = new ActiveAccount();
+            ac.setId(activeAccount.getId());
+            ac.setAccount(accountServices.searchAccounts(client));
+            ac.setActive(activeServices.findById(ac.getActive().getId()));
+            ac.setTotalActive(activeAccount.getTotalActive());
+            ac.setValorCompra(activeAccount.getValorCompra());
+            ac.setDateCreation(activeAccount.getDateCreation());
+            ac.setDateModification(activeAccount.getDateModification());
 
-                   ActiveAccount ac = new ActiveAccount();
-                   ac.setId(id);
-//                   ac.setConta(contaDAO.buscaContaPorId(idConta, clienteDAO));
-//                   ac.setAtivo(ativoDAO.buscaAtivoPorId(idAtivo));
-                   ac.setTotalActive(totalActive);
-                   ac.setValorCompra(valorCompra);
-                   ac.setDateCreation(dataCriacao);
-                   ac.setDateModification(dataModificacao);
-
-                   return ac;
-               }).collect(Collectors.toList());
+            return ac;
+        }).collect(Collectors.toList());
     }
 
     public ActiveAccount getAtivoContaById(Long id) {
